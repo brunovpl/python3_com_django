b3c988fa3f5bfa8361db0e3bfdd6c1b043077ca1
Bruno Verçosa Pereira Lins <brunovpl@gmail.com>
2018-01-04 13:01:25 -0300

    Adding more admin configuration to Courses

--- simplemooc/simplemooc/courses/admin.py
+++ simplemooc/simplemooc/courses/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
-from .models import  Course
+from .models import Course
 
-admin.site.register(Course)
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'start_date', 'create_at']
+    search_fields = ['name', 'slug']
+
+
+admin.site.register(Course, CourseAdmin)
