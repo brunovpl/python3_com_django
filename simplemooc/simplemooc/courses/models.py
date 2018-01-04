7e775912384bb09554ada3f64d752c7ee0d6bf1f
Bruno Verçosa Pereira Lins <brunovpl@gmail.com>
2018-01-03 22:42:21 -0300

    Working with migrations

--- simplemooc/simplemooc/courses/models.py
+++ simplemooc/simplemooc/courses/models.py
@@ -6,6 +6,6 @@ class Course(models.Model):
     slug = models.SlugField(verbose_name='Atalho')
     description = models.TextField(verbose_name='Descrição', blank=True)
     start_date = models.DateField(verbose_name='Data de Início', null=True, blank=True)
-    image = models.ImageField(verbose_name='Imagem', upload_to='courses/images')
+    image = models.ImageField(verbose_name='Imagem', upload_to='courses/images', null=True, blank=True)
     create_at = models.DateTimeField(verbose_name='Criado em', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='Atualizado em', auto_now=True)
