0a1a4fe235769eff582630fc3e113c32825f5496
Bruno Verçosa Pereira Lins <brunovpl@gmail.com>
2018-01-04 13:07:03 -0300

    Adding prepopulated fields

--- simplemooc/simplemooc/courses/admin.py
+++ simplemooc/simplemooc/courses/admin.py
@@ -6,6 +6,7 @@ from .models import Course
 class CourseAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'start_date', 'create_at']
     search_fields = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
 
 
 admin.site.register(Course, CourseAdmin)
