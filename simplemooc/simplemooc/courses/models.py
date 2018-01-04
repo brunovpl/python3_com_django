1f406586641e4e78215836af23019ad51370393c
Bruno Verçosa Pereira Lins <brunovpl@gmail.com>
2018-01-03 22:33:38 -0300

    Adding course model

---
+++ simplemooc/simplemooc/courses/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Course(models.Model):
+    name = models.CharField(verbose_name='Nome', max_length=100)
+    slug = models.SlugField(verbose_name='Atalho')
+    description = models.TextField(verbose_name='Descrição', blank=True)
+    start_date = models.DateField(verbose_name='Data de Início', null=True, blank=True)
+    image = models.ImageField(verbose_name='Imagem', upload_to='courses/images')
+    create_at = models.DateTimeField(verbose_name='Criado em', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='Atualizado em', auto_now=True)
