23ac4440b44a1e6ba4bcdfedba74d5686c612714
Bruno Verçosa Pereira Lins <brunovpl@gmail.com>
2018-01-04 21:00:43 -0300

    Showing courses list

--- simplemooc/simplemooc/courses/views.py
+++ simplemooc/simplemooc/courses/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from .models import Course
+
 
 def index(request):
-    return render(request=request, template_name='courses/index.html')
+    courses = Course.objects.all()
+    context = {
+        'courses': courses
+    }
+    return render(request=request, template_name='courses/index.html', context=context)
