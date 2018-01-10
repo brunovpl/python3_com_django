81fd84c6e8a0fc74eb4bdbe654b4f2ac44f26a38
Bruno Verçosa Pereira Lins <brunovpl@gmail.com>
2018-01-10 08:39:36 -0300

    Submiting a form

--- simplemooc/simplemooc/courses/views.py
+++ simplemooc/simplemooc/courses/views.py
@@ -14,14 +14,19 @@ def index(request):
 
 def details(request, slug):
     course = get_object_or_404(Course, slug=slug)
+    context = {}
 
     if request.method == 'POST':
         form = ContactCourse(request.POST)
+        if form.is_valid():
+            context['is_valid'] = True
+            print(form.changed_data)
+            form = ContactCourse()
+            print(form.changed_data)
     else:
         form = ContactCourse()
 
-    context = {
-        'course': course,
-        'form': form,
-    }
+    context['course'] = course
+    context['form'] = form
+
     return render(request=request, template_name='courses/details.html', context=context)
