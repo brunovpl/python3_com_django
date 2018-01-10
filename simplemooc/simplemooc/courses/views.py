3fce15dba44e6b25ac074860ca632eb7c8d5c240
Bruno Verçosa Pereira Lins <brunovpl@gmail.com>
2018-01-09 22:54:39 -0300

    Continue working with forms

--- simplemooc/simplemooc/courses/views.py
+++ simplemooc/simplemooc/courses/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from .forms import ContactCourse
 from .models import Course
 
 
@@ -11,9 +12,16 @@ def index(request):
     return render(request=request, template_name='courses/index.html', context=context)
 
 
-def details(request, pk):
-    course = Course.objects.get(pk=pk)
+def details(request, slug):
+    course = get_object_or_404(Course, slug=slug)
+
+    if request.method == 'POST':
+        form = ContactCourse(request.POST)
+    else:
+        form = ContactCourse()
+
     context = {
-        'course': course
+        'course': course,
+        'form': form,
     }
     return render(request=request, template_name='courses/details.html', context=context)
