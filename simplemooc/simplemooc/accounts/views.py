1237cd17677b597adb5fb2ee7b4a5d671e2d2a00
Bruno Verçosa Pereira Lins <brunovpl@gmail.com>
2018-01-19 10:25:55 -0300

    Login after singup

--- simplemooc/simplemooc/accounts/views.py
+++ simplemooc/simplemooc/accounts/views.py
@@ -1,4 +1,4 @@
-from django.conf import settings
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 
 from .forms import RegisterForm
@@ -9,8 +9,10 @@ def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect(settings.LOGIN_URL)
+            user = form.save()
+            user = authenticate(username=user.username, password=form.cleaned_data['password1'])
+            login(request=request, user=user)
+            return redirect('core:home')
     else:
         form = RegisterForm()
 
