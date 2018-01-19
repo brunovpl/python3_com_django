afead08e726ff108303dfdfca77a80be629bf776
Bruno Verçosa Pereira Lins <brunovpl@gmail.com>
2018-01-19 10:19:31 -0300

    Adding logout page

--- simplemooc/simplemooc/accounts/urls.py
+++ simplemooc/simplemooc/accounts/urls.py
@@ -5,5 +5,6 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('entrar/', auth_views.LoginView.as_view(template_name='accounts/login.html'),  name='login'),
+    path('sair/', auth_views.LogoutView.as_view(next_page='core:home'),  name='logout'),
     path('cadastre-se/', views.register,  name='register'),
 ]
