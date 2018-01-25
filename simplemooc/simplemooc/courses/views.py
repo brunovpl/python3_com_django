8235dd1a9b28e1e4166597ae923d26dbd481fd40
Bruno Verçosa Pereira Lins <brunovpl@gmail.com>
2018-01-24 22:31:19 -0300

    Add capability to cancel a course subscription

--- simplemooc/simplemooc/courses/views.py
+++ simplemooc/simplemooc/courses/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+
 from .forms import ContactCourse
 from .models import Course, Enrollment
-from django.contrib import messages
+
 
 def index(request):
     courses = Course.objects.all()
@@ -30,6 +32,7 @@ def details(request, slug):
 
     return render(request=request, template_name='courses/details.html', context=context)
 
+
 @login_required
 def enrollment(request, slug):
     course = get_object_or_404(Course, slug=slug)
@@ -40,10 +43,24 @@ def enrollment(request, slug):
         messages.success(request=request, message='Você foi inscrito no curso com sucesso')
     else:
         messages.info(request=request, message='Você já está inscrito no curso')
-        
-    
-    
+
     return redirect('accounts:dashboard')
+
+
+@login_required
+def undo_enrollment(request, slug):
+    course = get_object_or_404(Course, slug=slug)
+    enrollment = get_object_or_404(Enrollment, user=request.user, course=course)
+    if request.method == 'POST':
+        enrollment.delete()
+        messages.success(request=request, message='Sua inscrição foi cancelada com sucesso')
+        return redirect('accounts:dashboard')
+    template_name = 'courses/undo_enrollment.html'
+    context = {
+        'enrollment': enrollment,
+        'course': course,
+    }
+    return render(request=request, template_name=template_name, context=context)
 
 
 @login_required
@@ -59,5 +76,3 @@ def announcements(request, slug):
         'course': course
     }
     return render(request=request, template_name=template, context=context)
-
-
