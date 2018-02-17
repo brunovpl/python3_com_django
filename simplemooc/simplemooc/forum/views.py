7f90bad2b6c59ab9928c230fa6c169246e0a297a
Bruno Verçosa Pereira Lins <brunovpl@gmail.com>
2018-02-17 11:49:59 -0300

    Adding form to answer a thread

--- simplemooc/simplemooc/forum/views.py
+++ simplemooc/simplemooc/forum/views.py
@@ -1,5 +1,8 @@
+from django.contrib import messages
+from django.shortcuts import redirect
 from django.views.generic import ListView, DetailView
 
+from .forms import ReplyForm
 from .models import Thread
 
 
@@ -27,15 +30,37 @@ class ForumView(ListView):
         context['tags'] = Thread.tags.all()
         return context
 
+
 class ThreadView(DetailView):
     model = Thread
     template_name = 'forum/thread.html'
 
-
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ThreadView, self).get_context_data(**kwargs)
         context['tags'] = Thread.tags.all()
+        context['form'] = ReplyForm(self.request.POST or None)
         return context
+
+    def post(self, request, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            messages.error(
+                self.request,
+                'Para responder ao tópico é necessário estar logado'
+            )
+            return redirect(self.request.path)
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        form = context['form']
+        if form.is_valid():
+            reply = form.save(commit=False)
+            reply.thread = self.object
+            reply.author = self.request.user
+            reply.save()
+            messages.success(
+                self.request, 'A sua responsta foi enviada com sucesso'
+            )
+            context['form'] = ReplyForm()
+        return self.render_to_response(context)
 
 
 index = ForumView.as_view()
